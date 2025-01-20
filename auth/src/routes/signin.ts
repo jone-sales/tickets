9cd807af47f6b1bf8310dc87a6388ed83d38d265
jone-sales <maya_sales@outlook.com>
2025-01-20 13:52:33 -0300

    creates signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { validateRequest } from '../middlewares/validate-requests';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { PasswordHash } from '../utils/password-hash';
 
 const router = express.Router();
 
@@ -11,7 +15,33 @@ router.post('/api/users/signin',
         body("password").trim().notEmpty().withMessage("Password field cannot be empty.")
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials.')
+        }
+
+        const passwordMatch = await PasswordHash.compare(
+            existingUser.password,
+            password
+        )
+        if (!passwordMatch) {
+            throw new BadRequestError('Invalid credentials.')
+        }
+
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+            },
+            process.env.JWT_KEY!
+        );
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(200).send(existingUser);
     }
 );
 
