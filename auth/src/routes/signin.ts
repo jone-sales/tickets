b531c3867bd0fd62ffe7bc6b5159e77020ef2f00
jone-sales <maya_sales@outlook.com>
2025-01-20 13:08:12 -0300

    implements validateRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,18 @@
-import express from 'express'
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import { validateRequest } from '../middlewares/validate-requests';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Working.')
-});
+router.post('/api/users/signin', 
+    [
+        body("email").isEmail().withMessage("Email field must be a valid email adress."),
+        body("password").trim().notEmpty().withMessage("Password field cannot be empty.")
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
+    }
+);
 
 export { router as signinRouter };
