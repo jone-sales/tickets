74a9d278ed9323be350e73fd53f1ae0b8e844a8c
jone-sales <maya_sales@outlook.com>
2025-03-17 12:18:28 -0300

    update index with buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
-const LandingPage = () => {
-    return <h1>Landing Page??</h1>
+const LandingPage = ({ currentUser }) => {
+    return currentUser ? <h1>You signed in.</h1> : <h1>You are not signed in.</h1>
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined'){
-        const { data } = await axios.get(
-            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
-                headers: req.headers
-            }
-        );
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get("/api/users/currentuser");
 
-        return data;
-    } else {
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
+    return data;
 };
-
-
+   
 export default LandingPage;
