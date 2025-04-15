f5a3b18c7261cdbca4c08d610a24156a82c15feb
jone-sales <maya_sales@outlook.com>
2025-04-15 15:18:36 -0300

    creates base endpoints for order service

---
+++ orders/src/routes/cancel.ts
@@ -0,0 +1,33 @@
+import { NotFoundError, OrderStatus, UnauthorizedError } from '@js_tickets/common';
+import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
+const router = express.Router();
+
+router.patch('/api/orders/:orderId', async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    
+    if(!order) {
+        throw new NotFoundError();
+    }
+
+    if(order.userId != req.currentUser!.id) {
+        throw new UnauthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
+
+    res.send({order});
+});
+
+export { router as deleteOrderRouter };
